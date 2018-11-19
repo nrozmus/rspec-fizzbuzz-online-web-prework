991b5f1b3ed61d2dcf40ad78e5f43481a4213eec
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-19 22:20:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,5 @@
-def fizzbuzz(int)
- if int % 3 == 0 && int % 5 == 0
-   "FizzBuzz"
- elsif int % 3 == 0
-"Fizz"
-elsif int % 5 == 0
-  "Buzz"
-end
+def fizzbuzz(num)
+  return "FizzBuzz" if num % 5 == 0 && num % 3 == 0
+  return "Buzz" if num % 5 == 0
+  return "Fizz" if num % 3 == 0
 end
