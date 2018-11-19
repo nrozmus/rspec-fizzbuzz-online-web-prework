ed18e6da6dfd97a48f6bb3cf3b72dfee1f3ba828
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-19 22:23:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
-def fizzbuzz(fizzbuzz)
-  if int % 3 == 0 and int % 5 == 0
-    return "FizzBuzz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 3 == 0
-    return  "Fizz"
-   end
- end
+def fizzbuzz(int)
+  if int % 3==0 && int % 5==0
+    "FizzBuzz"
+  elsif int % 3==0
+    "Fizz"
+  elsif int % 5==0
+    "Buzz"
+  else
+    nil
+  end
+end
